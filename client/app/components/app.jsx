330fb676c02d2c2adaacfb6b49f7ca04533b8691
Bryan <cbfowler4@gmail.com>
2018-02-07 11:41:50 -0500

    front end structure setup

---
+++ client/app/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <div>yoyoyo</div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require("path");
+var webpack = require("webpack");
+
+// var plugins = []; // if using any plugins for both dev and production
+// var devPlugins = []; // if using any plugins for development
+//
+// var prodPlugins = [
+//   new webpack.DefinePlugin({
+//     'process.env': {
+//       'NODE_ENV': JSON.stringify('production')
+//     }
+//   })
+// ];
+
+// plugins = plugins.concat(
+//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
+
+module.exports = {
+  context: __dirname,
+  entry: "./client/app/index.js",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
+  },
+  // plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
